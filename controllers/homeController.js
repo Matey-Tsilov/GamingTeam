be2ea5922f74a96ae42e49e34748cb26bf358775
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-04-19 11:59:09 +0300

    implement the 404 error view to all errors

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,49 +6,78 @@ router.get("/", (req, res) => {
   res.render("home");
 });
 router.get("/catalog", async (req, res) => {
-  const games = await gameService.getAll();
-
-  res.render("catalog", { games });
+  try {
+    const games = await gameService.getAll();
+    res.render("catalog", { games });
+  } catch (error) {
+    res.render("404", error);
+  }
 });
 router.get("/catalog/:id", async (req, res) => {
-  const curGame = await gameService.getById(req.params.id);
+  try {
+    const curGame = await gameService.getById(req.params.id);
 
-    curGame.isGuest = req.user == undefined
+    curGame.isGuest = req.user == undefined;
     curGame.isOwner = req.user && curGame.owner == req.user._id;
-    curGame.hasBought = !curGame.isOwner && (curGame.boughtBy?.map(x => x.valueOf()).includes(req.user?._id));
+    curGame.hasBought =
+      !curGame.isOwner &&
+      curGame.boughtBy?.map((x) => x.valueOf()).includes(req.user?._id);
 
-  res.render("details", curGame);
-}); 
+    res.render("details", curGame);
+  } catch (error) {
+    res.render("404", error);
+  }
+});
 router.get("/create", isAuth, (req, res) => {
   res.render("create");
 });
 router.post("/create", isAuth, async (req, res) => {
-  const game = req.body;
-  await gameService.create(game, req.user._id);
+  try {
+    const game = req.body;
+    await gameService.create(game, req.user._id);
 
-  res.redirect("/catalog");
+    res.redirect("/catalog");
+  } catch (error) {
+    res.render("404", error);
+  }
 });
 router.get("/edit/:id", isAuth, async (req, res) => {
-  const curGame = await gameService.getById(req.params.id);
-  res.render("edit", curGame);
+  try {
+    const curGame = await gameService.getById(req.params.id);
+    res.render("edit", curGame);
+  } catch (error) {
+    res.render("404", error);
+  }
 });
 router.post("/edit/:id", isAuth, async (req, res) => {
-  const newGame = req.body;
-  const id = req.params.id;
+  try {
+    const newGame = req.body;
+    const id = req.params.id;
 
-  await gameService.edit(id, newGame);
-  res.redirect(`/catalog/${id}`);
+    await gameService.edit(id, newGame);
+    res.redirect(`/catalog/${id}`);
+  } catch (error) {
+    res.render("404", error);
+  }
 });
 router.get("/delete/:id", isAuth, async (req, res) => {
-  await gameService.delete(req.params.id);
-  res.redirect("/catalog");
+  try {
+    await gameService.delete(req.params.id);
+    res.redirect("/catalog");
+  } catch (error) {
+    res.render("404", error);
+  }
 });
 router.get("/buy/:id", isAuth, async (req, res) => {
-  const curGame = await gameService.getById(req.params.id);
-  curGame.boughtBy.push(req.user._id);
+  try {
+    const curGame = await gameService.getById(req.params.id);
+    curGame.boughtBy.push(req.user._id);
 
-  await gameService.edit(curGame._id, curGame);
-  res.redirect(`/catalog/${req.params.id}`);
+    await gameService.edit(curGame._id, curGame);
+    res.redirect(`/catalog/${req.params.id}`);
+  } catch (error) {
+    res.render("404", error);
+  }
 });
 
 module.exports = router;
