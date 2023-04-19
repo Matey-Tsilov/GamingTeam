1639626b1d342033755ccd07205aea23e363f013
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-04-19 12:11:14 +0300

    when invalid request -> 404 page

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const gameController = require('./controllers/gameController')
 const authController = require('./controllers/authController')
+const show404 = require('./controllers/show404')
 
 router.use(gameController)
 router.use('/auth', authController)
+router.use('/*', show404)
 
 module.exports = router
