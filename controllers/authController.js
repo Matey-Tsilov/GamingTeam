731d2bda9cc49f37c45fd382ca6d5ee54814827d
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-04-24 12:57:33 +0300

    MPA form validation comming from the js

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,21 +6,21 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-  
-    if (password != '' && email != '') {
-      try {
-        const user = await authService.login(email, password);
-        const token = await authService.generateToken(user);
-        res.cookie(COOKIE_SESSION_NAME, token, {httpOnly: true});
-        res.redirect("/");
-      } catch (error) {
-        res.render("login", { error: error.message });
-      }
-    }else{
-      res.render('login', {error: 'Email and passwrod inputs are mandatory!'})
+  const { email, password } = req.body;
+
+  if (password != "" && email != "") {
+    try {
+      const user = await authService.login(email, password);
+      const token = await authService.generateToken(user);
+      res.cookie(COOKIE_SESSION_NAME, token, { httpOnly: true });
+      res.redirect("/");
+    } catch (error) {
+      res.render("login", { error: error.message });
     }
-  });
+  } else {
+    res.render("login", { error: "Email and passwrod inputs are mandatory!" });
+  }
+});
 
 router.get("/register", (req, res) => {
   res.render("register");
@@ -31,22 +31,27 @@ router.post("/register", async (req, res) => {
 
   if (password !== rePass) {
     return res.render("register", { error: "Passwords mismatch!" });
-  }
-
-  try {
-    const user = await authService.create({ username, email, password });
-    const token = await authService.generateToken(user);
-    res.cookie(COOKIE_SESSION_NAME, token, {httpOnly: true});
-    res.redirect("/");
-  } catch (error) {
-    res.render("register", { error: error.message });
+  } else if (username.length < 5) {
+    return res.render("register", {error: "The username should be at least five characters long."})
+  } else if (email.length < 10) {
+    return res.render("register", {error: "The email should be at least ten character long."})
+  } else if (password.length < 4) {
+    return res.render("register", {error: "The password should be at least four characters long."})
+  } else {
+    try {
+      const user = await authService.create({ username, email, password });
+      const token = await authService.generateToken(user);
+      res.cookie(COOKIE_SESSION_NAME, token, { httpOnly: true });
+      res.redirect("/");
+    } catch (error) {
+      res.render("register", { error: error.message });
+    }
   }
 });
 
-router.get('/logout', (req, res) => {
-    res.clearCookie(COOKIE_SESSION_NAME)
-    res.redirect('/')
-})
-
+router.get("/logout", (req, res) => {
+  res.clearCookie(COOKIE_SESSION_NAME);
+  res.redirect("/");
+});
 
 module.exports = router;
