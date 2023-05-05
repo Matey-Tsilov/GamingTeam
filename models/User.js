8b419838e22ac2cb8e7b5a974bbbf0d6851f7bf8
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-05-05 12:47:45 +0300

    hash password with async/await syntax not Promise syntax

--- models/User.js
+++ models/User.js
@@ -9,26 +9,23 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: [true, 'The username is mandatory!'],
+        required: [true, 'The email is mandatory!'],
         unique: [true, 'This email already exists!'],
         minLength: [10, 'The email should be at least ten character long.']
     },
     password: {
         type: String,
-        required: [true, 'The username is mandatory!'],
+        required: [true, 'The password is mandatory!'],
         minLength: [4, 'The password should be at least four character long.']
     }
 })
 
-const User = mongoose.model('User', userSchema)
-
-userSchema.pre('save', function (next) {
-    bcrypt.hash(this.password, 10)
-    .then(hashedPass => {
-        this.password = hashedPass
-
-        next()
-    })
+userSchema.pre('save', async function (next) {
+    const saltRounds = 10
+    const hashedPass = await bcrypt.hash(this.password, saltRounds)
+    this.password = hashedPass
+    next()
 })
 
+const User = mongoose.model('User', userSchema)
 module.exports = User
